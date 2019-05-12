899b05901afd0d96711714de1ae0e9447c90742b
Sheikh Abdul Wahid Ahmed <sheikhg1900@gmail.com>
2019-05-13 00:26:50 +0500

    apps property added in di.js

--- razzle-plugins/di.js
+++ razzle-plugins/di.js
@@ -13,6 +13,19 @@ const getBasePath = () => path.resolve(__dirname, '../')
 const getDiConfig = () => require('../src/@di-config')
 const getAppPath = (appName = getDiConfig().runApp) =>  path.resolve(getBasePath(), `src/${appName}`)
 const getAppManifest = (appPath = getAppPath()) => require(path.resolve(appPath, '@manifest'))
+
+const getFiles = (dir, base = '') => readdirSync(dir, {withFileTypes: true}).reduce((files, file) => {
+    const filePath = path.join(dir, file.name)
+    const relativePath = path.join(base, file.name)
+    if(file.isDirectory()) {
+        return files.concat(getFiles(filePath, relativePath))
+    } else if(file.isFile()) {
+        file.__fullPath = filePath
+        file.__relateivePath = relativePath
+        return files.concat(file)
+    }
+}, [])
+
 const getDependencies = (manifest = getAppManifest(), dependencies = [getDiConfig().runApp]) => {
     const app = dependencies[dependencies.length - 1] 
     const dependencyNotDefined = !manifest.depends && app !== 'main'
@@ -25,29 +38,34 @@ const getDependencies = (manifest = getAppManifest(), dependencies = [getDiConfi
     dependencies.push(dependency)
     return getDependencies(getAppManifest(getAppPath(dependency)), dependencies)
     
-} 
-const getFiles = (dir, base = '') => readdirSync(dir, {withFileTypes: true}).reduce((files, file) => {
-    const filePath = path.join(dir, file.name)
-    const relativePath = path.join(base, file.name)
-    if(file.isDirectory()) {
-        return files.concat(getFiles(filePath, relativePath))
-    } else if(file.isFile()) {
-        file.__fullPath = filePath
-        file.__relateivePath = relativePath
-        return files.concat(file)
+}
+const dependencies = getDependencies()
+
+const getApps = () =>  dependencies.reduce((apps, dependency) => {
+    const path =  getAppPath(dependency)
+    apps[dependency] = { 
+        path, 
+        alias: `@${dependency}`,
+        files: getFiles(path).reduce((map, file) => {
+            map[file.__relateivePath] = file
+            return map
+        }, {})
     }
-}, [])
+    return apps
+}, {})
+const apps = getApps()
+
 const getAliasConfig = () => {
-    const aliasConfig = getDependencies().reduce((config, dependency) => {
-        config[`@${dependency}`] = getAppPath(dependency)
+    const aliasConfig = dependencies.reduce((config, dependency) => {
+        config[`@${dependency}`] =  getAppPath(dependency)
         return config
     }, {})
     aliasConfig['@'] = getAppPath("main")
     return aliasConfig
 }     
+const alias = getAliasConfig()
 
-module.exports.alias = getAliasConfig()
-module.exports.func = (config, { target, dev }, webpack, userOptions = {}) => {
+const func = (config, { target, dev }, webpack, userOptions = {}) => {
     console.log({dependencies:getDependencies(), alias:getAliasConfig()})
     config.plugins = [...config.plugins, new NormalModuleReplacementPlugin(/.*/, (resource) => {
         if (resource.request === '@/Home') {
@@ -58,3 +76,5 @@ module.exports.func = (config, { target, dev }, webpack, userOptions = {}) => {
     config.resolve.alias = {...config.resolve.alias, ...getAliasConfig()}
     return config
 }
+console.log(apps)
+module.exports = { alias, apps,  func }
