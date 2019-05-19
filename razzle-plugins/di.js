1c41b9b7dbeede1baa21d5b63d7099bad99b4f18
Sheikh Abdul Wahid Ahmed <sheikhg1900@gmail.com>
2019-05-19 15:20:10 +0500

    di work almost finished.

--- razzle-plugins/di.js
+++ razzle-plugins/di.js
@@ -1,6 +1,11 @@
-const {resolve, relative, join, sep} = require('path')
+const { resolve, relative, join, sep } = require('path')
 const { lstatSync, readdirSync, statSync } = require('fs')
 const { NormalModuleReplacementPlugin } = require('webpack')
+const debug = require('debug')
+
+
+debug.enable('di:info')
+const info = debug('di:info')
 
 const throwif = (condition, error = new Error('unknown error')) => {
     if (condition) {
@@ -10,9 +15,9 @@ const throwif = (condition, error = new Error('unknown error')) => {
 }
 const MAIN_APP = '@main'
 const BASE_PATH = resolve(__dirname, '../')
-const DI_CONFIG = require('../src/@di-config')
+const DI_CONFIG = require('../src/apps/@di-config')
 
-const getAppPath = (appName) => resolve(BASE_PATH, `src/${appName}`)
+const getAppPath = (appName) => resolve(BASE_PATH, `src/apps/${appName}`)
 const MAIN_APP_PATH = getAppPath(MAIN_APP)
 
 const getAppManifest = (appPath) => require(resolve(appPath, '@manifest'))
@@ -28,12 +33,13 @@ const getFiles = (dir, base = '') => readdirSync(dir, { withFileTypes: true }).r
         file.__relateivePath = relativePath
         return files.concat(file)
     }
+    return files
 }, [])
 
 const getDependencies = (manifest = RUN_APP_MANIFEST, dependencies = [DI_CONFIG.runApp]) => {
     const app = dependencies[dependencies.length - 1]
     const dependencyNotDefined = !manifest.depends && app !== MAIN_APP
-    const dependency = throwif(dependencyNotDefined, new Error(`The app '${app}' should define 'depends' field in @manifest.js. hint {depends:'main'}`)) || manifest.depends
+    const dependency = throwif(dependencyNotDefined, new Error(`The app '${app}' should define 'depends' field in @manifest.js. hint {depends:'@main'}`)) || manifest.depends
     if (!dependency) return dependencies
 
     throwif(dependencies.find((d) => d === dependency), new Error(`dependency cycle detected. {depends:'${dependency}'}' should not be in @manifest.js of the app, '${app}'.
@@ -44,14 +50,13 @@ const getDependencies = (manifest = RUN_APP_MANIFEST, dependencies = [DI_CONFIG.
 
 }
 const dependencies = getDependencies().reverse()  //top to bottom
-
 const getApps = () => dependencies.reduce((apps, dependency, index, source) => {
     const path = getAppPath(dependency)
     apps[dependency] = {
         level: index,
         path,
         alias: dependency,
-        app: dependency,
+        name: dependency,
         files: getFiles(path).reduce((map, file) => {
             map[file.__relateivePath] = file
             return map
@@ -76,6 +81,7 @@ const getAliasConfig = () => {
 }
 const alias = getAliasConfig()
 
+// eslint-disable-next-line compat/compat
 const getPathApp = (path) => Object.values(apps).find((app) => path === app.path || path.startsWith(app.path + sep))
 
 const resolveModule = (moduleId, basePath = BASE_PATH) => {
@@ -107,21 +113,31 @@ const resolveModule = (moduleId, basePath = BASE_PATH) => {
     return module
 }
 
-const m = resolveModule('@/Home','E:\\work\\github\\with-razzle\\src\\@main')
-console.log({m, sep:sep, apps, app: getPathApp('E:\\work\\github\\with-razzle\\src\\@main')})
-
-const dependencyInjector = (resource) => {
-    if (resource.request === '@/Home') {
-        const module = resolveModule(resource.request, resource.context)
-        if(module) {
-            const contextApp = getPathApp(resource.context)
-            console.log({contextApp, module, MAIN_APP, BASE_PATH, DI_CONFIG, MAIN_APP_PATH, RUN_APP_MANIFEST, dependencies, apps, alias, resource, context: resource.context, request: resource.request })
+const getCustomizing = (module, contextApp) => {
+    for (let i = dependencies.length - 1; i > contextApp.level; i--) {
+        const dependency = dependencies[i]
+        const app = apps[dependency]
+        let file = null
+        for (let ext of ['', '.js', '.jsx', 'ts', 'tsx']) {
+            const relativePath = module.appRelativePath + ext
+            file = app.files[relativePath]
+            if (file) {
+                return {file, app}
+            }
         }
-        
-        // resource.request = '@aw/Home'
     }
 }
 
+const dependencyInjector = (resource) => {
+    let module, contextApp, customizing
+    if ((module = resolveModule(resource.request, resource.context))
+        && (contextApp = getPathApp(resource.context))
+        && (customizing = getCustomizing(module, contextApp))) {
+        const { file, app } = customizing
+        resource.request = file.__fullPath
+        info(`[${contextApp.name}] [${module.app.name}->${app.name}] ${file.__relateivePath}`)
+    }
+}
 
 const func = (config, { target, dev }, webpack, userOptions = {}) => {
     config.plugins = [...config.plugins, new NormalModuleReplacementPlugin(/.*/, dependencyInjector)]
