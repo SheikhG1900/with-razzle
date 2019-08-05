a99a269862c87d301658ebfddf6841305b771cdc
Sheikh Abdul Wahid Ahmed <sheikhg1900@gmail.com>
2019-08-05 14:48:56 +0500

    server file with no render. (server.tsx alternative)

---
+++ src/apps/@main/server-no-render.tsx
@@ -0,0 +1,50 @@
+import { ChunkExtractor } from '@loadable/server'
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import cors from 'cors'
+import express from 'express'
+import stats from '../../../build/react-loadable.json'
+
+import controllers from '@/server/controllers'
+import { PUBLIC_DIR } from '_/env'
+
+const server = express()
+const extractor = new ChunkExtractor({ stats, entrypoints: ['client'] })
+const configuredCors = cors()
+
+server
+    .disable('x-powered-by')
+    .use(bodyParser.json({ limit: '20mb' }))
+    .use(bodyParser.urlencoded({ limit: '20mb', extended: false }))
+    .use(compression())
+    .use(express.static(PUBLIC_DIR))
+    .use('/api', configuredCors, controllers)
+    .get('/*', (req, res) => {
+        console.log('react endpoint called')
+
+        const sendResponse = (preLoadedState) => {
+            res.status(200).send(
+                `<!doctype html>
+<html lang="">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charset="utf-8" />
+        <title>Welcome to Razzle</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${extractor.getStyleTags()}
+        ${extractor.getScriptTags()}
+    </head>
+    <body>
+        <div id="root"></div>
+        <script>
+            window.__PRE_LOADED_STATE__ = ${preLoadedState}
+        </script>
+    </body>
+</html>`
+            )
+        }
+
+        sendResponse('undefined')
+    })
+
+export default server
